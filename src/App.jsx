92b12689a6a1a488cbbeb2ee504c079071ef67c4
Salar Hafezi <salar.hfz.work@gmail.com>
2023-01-30 21:41:35 +0330

    wire up app - iteration #1

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
+import { useEffect, useState } from 'react'
+import Home from './components/Home'
+import Quiz from './components/Quiz'
+
 import './App.scss'
+import useFetch from './hooks/useFetch'
 
 function App() {
+  const [currentPage, setCurrentPage] = useState(localStorage.getItem('currentPage') || 'home')
+  const fetchResult = useFetch('https://opentdb.com/api.php?amount=10&category=23&difficulty=easy&type=multiple')
+
+  useEffect(() => {
+    localStorage.setItem('currentPage', currentPage)
+  }, [currentPage])
+
+  function togglePage(page) {
+    setCurrentPage(page)
+  }
+
   return (
-    <h1>App</h1>
+    <div className="container">
+      {currentPage === 'home' ? <Home togglePage={togglePage} {...fetchResult} /> : <Quiz togglePage={togglePage} {...fetchResult} />}
+    </div>
+
   )
 }
 
